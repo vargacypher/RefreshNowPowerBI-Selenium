61806f761b4abdb299c4933a3a3f9440f5ad7d10
Guilherme Cardoso de Vargas <77084039+vargacypher@users.noreply.github.com>
2021-10-26 15:26:12 -0300

    Update update_reports_BI.py

--- update_reports_BI.py
+++ update_reports_BI.py
@@ -1,52 +1,82 @@
+# -*- coding: utf-8 -*-
+__author__ = 'Guilherme Cardoso de Vargas'
+__version__ = 1.0
+__maintainer__ = 'Guilherme Cardoso de Vargas'
+__email__ = 'vargas93626@gmail.com'
+__status__ = 'Development'
+
+
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
 import time
+
 
 def main():
     '''LINK PARA LOGIN NO POWER BI'''
     powerbi = 'https://app.powerbi.com/home?redirectedFromSignup=1&noSignUpCheck=1&response=AlreadyAssignedLicense'
 
     '''CONFIGURAÇÕES CHROME DRIVE'''
-
-    #(NO browser window)
+    
     option = webdriver.ChromeOptions()
+    #(NO browser window)
     #option.headless = True
     #option.incognito = True
 
     #BROWSER OBJECT / NAVIGATE TO POWER BI / MAXIMIZE WINDOW 
-    browser = webdriver.Chrome(executable_path='C:\\Users\\vargas.guilherme\\Desktop\\ScriptsPy\\update_reports_BI\\chromedriver' , options=option)
+    browser = webdriver.Chrome(executable_path='chromedriver.exe' , options=option)
     browser.get(powerbi)
+    action = ActionChains(browser)
     browser.maximize_window()
 
     #INSERE O EMAIL E CLICA EM NEXT
     time.sleep(4)
     bi_email = WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.ID, 'i0116'))) 
-    bi_email.send_keys('your email')
+    bi_email.send_keys('EMAIL')
     bi_email.send_keys(Keys.ENTER)
 
     #INSERE A SENHA E CLICA EM ENTRAR
     time.sleep(4)
     bi_pass = WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.ID, 'i0118')))
-    bi_pass.send_keys('your password')
+    bi_pass.send_keys('PASSWORD')
     bi_pass.send_keys(Keys.ENTER)
 
-    '''THIS IS THE PROMPT THAT ASKS IF YOU WANT TO REMAIN LOGGED IN'''
-    '''WAIT / STAY ON CURRENT SELECTION / MOVE AND CLICK ON ADJACENT SELECTION'''
+# Clica para nao se manter conectado
     time.sleep(4)
     yes_choice = browser.find_element_by_id('idSIButton9')
     yes_choice.send_keys(Keys.SHIFT, Keys.TAB, Keys.ENTER)
 
-    #LOCALIZA A WORKSPACE E CLICA NELA
+    #LOCALIZA A WORKSPACE 
     time.sleep(4)
-    browser.get('https://app.powerbi.com/groups/72074a89-eb16-4e08-80f9-1c94fb7c1c4e/reports/c170d176-604a-4c9a-b4d3-fa3914eb0a16/ReportSection7797ffc916e2b27c645e')
+    browser.get('https://app.powerbi.com/groups/72074a89-eb16-4e08-80f9-1c94fb7c1c4e/list')
 
-    #Atualiza o report
-    time.sleep(10)
-    refresh = browser.find_element_by_id('reportAppBarRefreshBtn').click()
+    #Vai até a DIV para localizar o botão de att
+    time.sleep(5)
+    div_elem = WebDriverWait(browser, 20).until(
+        EC.presence_of_element_located((By.XPATH, "//div[@class='cdk-virtual-scroll-content-wrapper']"))
+    )
+
+    #Localização da base a ser atualizada através do elemento com seu index
+    att_button = browser.find_elements_by_css_selector(".row.ng-star-inserted")[1] #index
+
+    
+    #Mimetiza mouse
+    action.move_to_element(att_button)
+
+    action.perform()
+    
+    print("deu bom")
+    
+    time.sleep(3)
+    buttonatualizarpainel_elem = WebDriverWait(browser, 20).until(
+        EC.presence_of_element_located((By.XPATH, "//button[@title='Atualizar agora']"))
+    )
+    
+    buttonatualizarpainel_elem.click()
     
     print ('Dataset Atualizado')
 
